b86cca0b209b6efe72ed158964031522ff77e1df
Andrew DeCarlo <andydecarlo@gmail.com>
2021-01-09 20:41:00 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const donate = prompt("Would you like to donate to the New River Humane Society? (Y/N)")
+
+if (donate.toUpperCase() === "Y" || donate.toUpperCase() === "YES") {
+    window.location.href = "donate.html";
+}
